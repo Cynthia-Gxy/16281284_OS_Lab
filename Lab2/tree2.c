f5e1d508eb969bac21b33ce086d9374b08386162
16281284 <568335726@qq.com>
2019-03-22 14:15:41 +0800

    循环输出自己的ID和父进程的ID

---
+++ Lab2/tree2.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    pid_t p1,p2,pp;// 记录生成的两个子进程的PID
+    p1=fork();
+    if(p1==0)
+    {
+ 	p2=fork();
+	if(p2>0)
+	pp=fork();
+while(1){
+	printf("进程%d的爸爸是:进程%d\n",getpid(),getppid());
+	sleep(1);
+}
+    }
+    if(p1>0)
+    {
+ 	fork();
+while(1){
+	printf("进程%d的爸爸是:进程%d\n",getpid(),getppid());
+	sleep(1);
+}
+    }
+	return 0;
+}
