178f52105047180092089dbe8af6cae79c6ce093
16281284 <568335726@qq.com>
2019-03-22 14:01:38 +0800

    Create mem.c

---
+++ Lab1/mem.c
@@ -0,0 +1,18 @@
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <assert.h>
+ int main()
+ {
+ int *p = malloc(sizeof(int)); // a1
+ assert(p != NULL);
+ printf("(%d) address pointed to by p: %p\n",
+ getpid(), p); // a2
+ *p = 0; // a3
+ while (1) {
+	 sleep(1);
+	 *p = *p + 1;
+	 printf("(%d) p: %d\n", getpid(), *p); // a4
+ }
+ return 0;
+}
